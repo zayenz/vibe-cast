bdc4ddd53f93280b5e1c918616df2c93fae61e27
Mikael Zayenz Lagerkvist <zayenz@gmail.com>
2026-01-11 15:09:27 +0100

    test: update slideshow removal test to reflect schema changes

--- src/plugins/visualizations/__tests__/PhotoSlideshowPluginRemoval.test.tsx
+++ src/plugins/visualizations/__tests__/PhotoSlideshowPluginRemoval.test.tsx
@@ -2,13 +2,8 @@ import { describe, it, expect } from 'vitest';
 import { PhotoSlideshowPlugin } from '../PhotoSlideshowPlugin';
 
 describe('PhotoSlideshowPlugin Feature Removal', () => {
-  it('should not have Apple Photos as a source option', () => {
+  it('should not have sourceType setting (it has been removed)', () => {
     const sourceTypeSetting = PhotoSlideshowPlugin.settingsSchema.find(s => s.id === 'sourceType');
-    expect(sourceTypeSetting).toBeDefined();
-    
-    if (sourceTypeSetting?.type === 'select') {
-      const options = sourceTypeSetting.options.map(o => o.value);
-      expect(options).not.toContain('photos');
-    }
+    expect(sourceTypeSetting).toBeUndefined();
   });
 });
